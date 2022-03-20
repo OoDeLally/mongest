8f1f6d874097b95ef07cb8f54b0e6a42c3735855
Pascal Heitz <oodelally.npm@gmail.com>
2022-03-20 15:03:42 +0100

    export types

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { BuildMongestService } from './BuildMongestService';
 export { MongestService } from './MongestService';
 export { isEntityInstanceOf } from './polymorphism';
 export { registerEntityClassForSchema } from './registerEntityClassForSchema';
+export { EntityPayload, AbstractType } from './types';
--- src/types.ts
+++ src/types.ts
@@ -2,8 +2,8 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import { ObjectId } from 'mongodb';
 
-// Do NOT use EntityPayload = Record<string, unknown>. Even if in practice it is the case,
-// TS considers that classes can be indexed by non-string keys.
+// Do NOT use EntityPayload = Record<string, unknown>.
+// Even if in practice it is not the case TS considers that classes can be indexed by non-string keys.
 export type EntityPayload = object;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
